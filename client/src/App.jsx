3de9a12197c7f42c4d806f8e9246d63ee657ead8
Lovely Sharma <lovelysingeras297@gmail.com>
2023-11-28 19:07:00 +0530

    Created sections pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
+import {
+  Hero,
+  Services,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Subscribe,
+  SuperQuality,
+  SpecialOffer,
+} from "./sections/index";
+
 const App = () => {
-  return <div className="text-3xl bg-green-800">App</div>;
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x bg-black padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 };
 
 export default App;
