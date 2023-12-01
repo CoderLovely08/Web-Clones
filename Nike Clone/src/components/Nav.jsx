0fb0375c3828e7bf8472aa5f758e585863dcacea
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-01 23:30:14 +0530

    Added responsive navbar

--- Nike Clone/src/components/Nav.jsx
+++ Nike Clone/src/components/Nav.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleMenuToggle = () => {};
+
   return (
     <header className="padding-x py-4 sticky top-0 z-50 w-full shadow-md bg-white">
       <nav className="flex justify-between items-center max-container">
@@ -28,9 +32,30 @@ const Nav = () => {
           })}
         </ul>
 
+        {isMenuActive && (
+          <div className="bg-white shadow-lg rounded-b-md absolute w-full h-auto top-16 left-0">
+            <ul className="flex-1 flex flex-col justify-center items-center gap-8 py-16">
+              {navLinks.map((item) => {
+                return (
+                  <li key={item.label}>
+                    <a
+                      onClick={() => setIsMenuActive(false)}
+                      href={item.href}
+                      className="font-montserrat leading-normal text-2xl font-bold text-slate-500 underline "
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+
         {/* Hamburger */}
         <div className="hidden max-lg:block">
           <img
+            onClick={() => setIsMenuActive((prev) => !isMenuActive)}
             src={hamburger}
             alt="hamburger icon"
             height={25}
