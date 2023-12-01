70b5f7eb38168d2d4815d6ea58b083cc218ee184
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-01 16:32:38 +0530

    Completed super quality section

--- client/src/sections/SuperQuality.jsx
+++ client/src/sections/SuperQuality.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { arrowRight } from "../assets/icons";
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you{" "}
+          <span className="text-coral-red inline-block mt-3"> Super</span>
+          <br></br>
+          <span className="text-coral-red inline-block mt-3">
+            Quality{" "}
+          </span>{" "}
+          Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
 
-export default SuperQuality
+        {/* Shop Button */}
+        <div className="mt-11">
+          <Button label={"View Details"} iconURL={arrowRight} />
+        </div>
+      </div>
+
+      <div className="flex flex-1 justify-center items-center">
+        <img src={shoe8}
+          alt="shoe"
+        ></img>
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
