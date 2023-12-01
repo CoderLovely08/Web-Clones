20b081b1ef9730097df75210ae53b53f067d781a
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-01 19:33:36 +0530

    Typo fix

--- Nike Clone/src/sections/CustomerReviews.jsx
+++ Nike Clone/src/sections/CustomerReviews.jsx
@@ -6,7 +6,7 @@ const CustomerReviews = () => {
   return (
     <section className="max-container">
       <h3 className="font-palanquin text-center font-bold  text-4xl">
-        What our <span className="text-coral-red">Cutomsers</span> say?
+        What our <span className="text-coral-red">Customers</span> say?
       </h3>
       <p className="info-text m-auto mt-4 max-w-lg text-center"></p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
